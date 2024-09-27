50243dc1a4adb94a7dda7288f5e1ffaef04c7016
KBbayron <bayronlr@outlook.com>
2024-09-26 19:30:54 -0600

    arreglo clase 3

--- src/app/componentes/agregar-empleado/agregar-empleado.component.ts
+++ src/app/componentes/agregar-empleado/agregar-empleado.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
-
+import { FormGroup, FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-agregar-empleado',
   templateUrl: './agregar-empleado.component.html',
   styleUrl: './agregar-empleado.component.css'
 })
 export class AgregarEmpleadoComponent {
+  formularioDeEmpleados: FormGroup;
+  constructor (public formulario: FormBuilder){
+    this.formularioDeEmpleados = this.formulario.group({
+      nombre: [''],
+      correo: ['']
+    })
+  }
   // cualquier tipo de dato
   enviarDatos (): any{
-    console.log('se preciono el boton')
+    console.log('se preciono el boton');
+    console.log(this.formularioDeEmpleados.value);
   }
 }
